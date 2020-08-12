38347ef76d5f212caea99a3c30ff0baef7097047
Akhil Reddy <akhilreddychalla98@gmail.com>
2020-08-12 22:57:37 +0530

    dynamic Drawing implemented

--- src/board/Board.jsx
+++ src/board/Board.jsx
@@ -2,15 +2,43 @@ import React, { Component } from "react";
 import "./cssFiles/css_for_board.css";
 
 class Board extends Component {
-  changeColorOfBars = () => {
+  constructor() {
+    super();
+    this.state = {
+      mouseButtonPressed: false,
+    };
+  }
+  clearAll = () => {
     //console.log("here", document.getElementById("0"));
     for (let i = 0; i < rows_count; i++) {
       for (let j = 0; j < columns_count; j++) {
         let node = i.toString() + "," + j.toString();
         //console.log(node);
         let table_data = document.getElementById(node);
-        table_data.setAttribute("class", "colorfulBox");
+        table_data.setAttribute("class", "box");
       }
+    }
+  };
+  mouseButtonClicked = (node) => {
+    this.setState({ mouseButtonPressed: true });
+    let table_data = document.getElementById(node);
+    if (grid_array[node[0]][node[1]] === 1) {
+      //Already Colored
+      grid_array[node[0]][node[1]] = 0; //De-color it
+      table_data.setAttribute("class", "box");
+    } else {
+      table_data.setAttribute("class", "colorfulBox");
+      grid_array[node[0]][node[1]] = 1;
+    }
+  };
+  mouseBtnReleased = () => {
+    this.setState({ mouseButtonPressed: false });
+  };
+  changeColorOfNode = (node) => {
+    if (this.state.mouseButtonPressed) {
+      let table_data = document.getElementById(node);
+      table_data.setAttribute("class", "colorfulBox");
+      grid_array[node[0]][node[1]] = 1;
     }
   };
   render() {
@@ -18,20 +46,29 @@ class Board extends Component {
       <div>
         <div className="header_box">
           <p>Path-Finding Visualizer</p>
-          <button className="cta" onClick={() => this.changeColorOfBars()}>
-            Color-All!
+          <button className="cta" onClick={() => this.clearAll()}>
+            Clear-Board!
           </button>
         </div>
         <div className="container">
           <table>
             <tbody>
-              {array.map((row, row_number) => (
+              {grid_array.map((row, row_number) => (
                 <tr id={row_number} key={row_number}>
                   {row.map((col, col_number) => (
                     <td
                       className="box"
                       id={[row_number, col_number]}
                       key={col_number}
+                      onMouseDown={() =>
+                        this.mouseButtonClicked([row_number, col_number])
+                      }
+                      onMouseUp={() =>
+                        this.mouseBtnReleased([row_number, col_number])
+                      }
+                      onMouseEnter={() =>
+                        this.changeColorOfNode([row_number, col_number])
+                      }
                     ></td>
                   ))}
                 </tr>
@@ -48,10 +85,9 @@ const screen_width = window.innerWidth;
 const screen_height = window.innerHeight;
 const new_s_w = screen_width - 20; //Subtract 2px from right & 2px from left
 const new_s_h = screen_height - 120; //Preserve 100px from top for Logo, 2px from bottom
-const array = [];
+const grid_array = [];
 const divide_by = 24; //Box occupies 18px+1px(border)+1px(padding)+gap b/w rows,cols
 const rows_count = Math.floor(new_s_h / divide_by); //Number of rows
 const columns_count = Math.floor(new_s_w / divide_by); //Number of columns
-console.log("r_c, c_c: " + rows_count + " " + columns_count);
 for (let i = 0; i < rows_count; i++)
-  array.push(new Array(columns_count).fill(0));
+  grid_array.push(new Array(columns_count).fill(0));
